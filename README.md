6603ebc949cc08e02de84db21b066c075876a3dc
vigvijay <vigvijay@indiana.edu>
2016-03-24 03:10:12 -0400

    wireframe set up for node.js web application with mysql connection

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # z511-pubmed_search_engine
 Repository for web application search engine for machine reading of diseases.
+
+1. Run npm install in webapps folder
+2. Install nodemon to monitor the server.
+3. Run server using command "nodemon app.js"
+
+
+Team Members:
+* Venkateh Thirupathisamy
+* Vineeta Sangaraju
+* Vignesh Vijayaraghavan
---
+++ webapp/app.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+//var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+var mysql      = require('mysql');
+
+var pool      =    mysql.createPool({
+    connectionLimit : 100, //important
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '*****',
+    database : 'database_design',
+    debug    :  false
+});
+
+function handle_database(req,res) {
+
+    pool.getConnection(function(err,connection){
+        if (err) {
+          connection.release();
+          res.json({"code" : 100, "status" : "Error in connection database"});
+          return;
+        }
+
+        console.log('connected as id ' + connection.threadId);
+
+        connection.query("select * from genes LIMIT 2",function(err,rows){
+            connection.release();
+            if(!err) {
+                res.json(rows);
+            }
+        });
+
+        connection.on('error', function(err) {
+              res.json({"code" : 100, "status" : "Error in connection database"});
+              return;
+        });
+  });
+}
+
+//app.use('/', routes);
+//app.use('/users', users);
+
+// catch 404 and forward to error handler
+/*app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+*/
+app.get('/', function(req, res) {
+   res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+app.get('/sqlOutput', function(req, res) {
+   handle_database(req,res);
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+module.exports = app;
