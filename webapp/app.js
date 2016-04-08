ab22da12766ac489699e187a9c1d051f04a23572
vigvijay <vigvijay@indiana.edu>
2016-04-07 21:15:36 -0400

    Having fixed header for the results page

--- webapp/app.js
+++ webapp/app.js
@@ -211,6 +211,6 @@ app.get('/sqlOutput', function(req, res) {
    handle_database(req,res);
 });
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Application now listening on port 3000!');
 });
 module.exports = app;
