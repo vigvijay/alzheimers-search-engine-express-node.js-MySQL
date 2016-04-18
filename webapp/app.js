fc43372dc80a2ff1d45cb01378c50af7f0ad783e
vigvijay <vigvijay@indiana.edu>
2016-04-18 01:15:53 -0400

    to highlight selection in results page

--- webapp/app.js
+++ webapp/app.js
@@ -21,153 +21,169 @@ app.set('view engine', 'html');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/views')));
 
 
-var mysql      = require('mysql');
+var mysql = require('mysql');
 
-var pool      =    mysql.createPool({
-    connectionLimit : 100, //important
-    host     : 'beat.comp.ae.keio.ac.jp',
-    port     :  *,
-    user     : 'user',
-    password : '*******'
-    database : 'ad_reverb',
-    debug    :  false
+var pool = mysql.createPool({
+    connectionLimit: 100, //important
+    host: 'beat.comp.ae.keio.ac.jp',
+    port: 49155,
+    user: 'user',
+    password: 'yamaguti',
+    database: 'ad_reverb',
+    debug: false
 });
 
 function handle_database(req, res) {
 
-    pool.getConnection(function(err,connection){
+    pool.getConnection(function(err, connection) {
         if (err) {
-          connection.release();
-          res.json({"code" : 100, "status" : "Error in connection database"});
-          return;
+            connection.release();
+            res.json({
+                "code": 100,
+                "status": "Error in connection database"
+            });
+            return;
         }
 
         console.log('connected as id ' + connection.threadId);
 
-       // console.log(req);
+        // console.log(req);
         // hardcoding input data at the moment
         var sentence_triple1 = req.query.triple1;
-		var sentence_triple2 = req.query.triple2;
-		var sentence_triple3 = req.query.triple3;
+        var sentence_triple2 = req.query.triple2;
+        var sentence_triple3 = req.query.triple3;
 
-		//Parsing the triples based on ";"
-		var triple1_synonymns = sentence_triple1.split(';');
-		var triple2_synonymns = sentence_triple2.split(';');
-		var triple3_synonymns = sentence_triple3.split(';');
+        //Parsing the triples based on ";"
+        var triple1_synonymns = sentence_triple1.split(';');
+        var triple2_synonymns = sentence_triple2.split(';');
+        var triple3_synonymns = sentence_triple3.split(';');
 
-		var triple1_query="(";
-		for(i=0;i<triple1_synonymns.length;i++){
-			if(i==triple1_synonymns.length-1)
-				triple1_query+=" s=?";
-			else
-				triple1_query+=" s=? or";
-		}
-		triple1_query+=")";
+        var triple1_query = "(";
+        for (i = 0; i < triple1_synonymns.length; i++) {
+            if (i == triple1_synonymns.length - 1)
+                triple1_query += " s=?";
+            else
+                triple1_query += " s=? or";
+        }
+        triple1_query += ")";
 
-		var triple2_query="(";
-		for(i=0;i<triple2_synonymns.length;i++){
-			if(i==triple2_synonymns.length-1)
-				triple2_query+=" p=?";
-			else
-				triple2_query+=" p=? or";
-		}
-		triple2_query+=")";
+        var triple2_query = "(";
+        for (i = 0; i < triple2_synonymns.length; i++) {
+            if (i == triple2_synonymns.length - 1)
+                triple2_query += " p=?";
+            else
+                triple2_query += " p=? or";
+        }
+        triple2_query += ")";
 
-		console.log(triple2_query);
+        console.log(triple2_query);
 
-		//To modify after understanding what to do with triple3
-		/*var triple3_query="(";
-		for(i=0;i<triple3_synonymns.length;i++){
-			if(i==triple3_synonymns.length-1)
-				triple3_query+=" p=?";
-			else
-				triple3_query+=" p=? or";
-		}
-		triple3_query+=")";*/
+        //To modify after understanding what to do with triple3
+        /*var triple3_query="(";
+        for(i=0;i<triple3_synonymns.length;i++){
+        	if(i==triple3_synonymns.length-1)
+        		triple3_query+=" p=?";
+        	else
+        		triple3_query+=" p=? or";
+        }
+        triple3_query+=")";*/
 
-        connection.query("select s,p,o, count(o) as c from triples where "+ triple1_query +" and "+ triple2_query +" Group by o ORDER BY count(o) DESC"
-        , (triple1_synonymns.concat(triple2_synonymns)),function(err,rows){
+        connection.query("select s,p,o, count(o) as c from triples where " + triple1_query + " and " + triple2_query + " Group by o ORDER BY count(o) DESC", (triple1_synonymns.concat(triple2_synonymns)), function(err, rows) {
             connection.release();
-            if(!err) {
-              var arr = [];
-              for(var dataObj in rows){
-                arr.push({
-                  'key':rows[dataObj].o,
-                  'value':rows[dataObj].c,
-                  'subject':rows[dataObj].s,
-                  'predicate':rows[dataObj].p
-                });
-              }
+            if (!err) {
+                var arr = [];
+                for (var dataObj in rows) {
+                    arr.push({
+                        'key': rows[dataObj].o,
+                        'value': rows[dataObj].c,
+                        'subject': rows[dataObj].s,
+                        'predicate': rows[dataObj].p,
+                        'inputSubject': sentence_triple1,
+                        'inputPredicate': sentence_triple2,
+                        'inputObject': sentence_triple3
+                    });
+                }
 
 
                 //rows.sort(function(a, b) {return a[0] - b[0]})
                 //res.json(rows);
-                res.render('results', { result: JSON.stringify(arr) });
+                res.render('results', {
+                    result: JSON.stringify(arr)
+                });
                 //res.end();
                 //return rows;
+            } else {
+                console.log(err);
             }
-            else{
-              console.log(err);
-            }
-          });
+        });
 
         connection.on('error', function(err) {
-              res.json({"code" : 100, "status" : "Error in connection database"});
-              return;
-        });
-  });
-}
-app.get('/q', function (req, res) {
-   handle_database(req, res);
-});
-
-app.get('/get-link', function (req, res) {
-  pool.getConnection(function(err,connection){
-      if (err) {
-        connection.release();
-        res.json({"code" : 100, "status" : "Error in connection database"});
-        return;
-      }
-      console.log('connected as id ' + connection.threadId);
-
-      var where_clause = " ";
-      if(req.query.filter == "true"){
-        where_clause += " and year = " + req.query.year;
-      }
-
-      var queryEntries = [];
-      queryEntries.push(req.query.sub);
-      queryEntries.push(req.query.pred);
-      queryEntries.push(req.query.key );
-      connection.query("select s,p,o, triples.pmid, sentence, year from  triples, sentences, years where s = ? " + where_clause + " and p= ? and o= ? and triples.sent_id=sentences.sent_id and triples.pmid = years.pmid"
-      , (queryEntries), function(err,rows){
-          connection.release();
-          if(!err) {
-            var arr = [];
-            for(var dataObj in rows){
-              arr.push({
-                'key':rows[dataObj].sentence,
-                'value':"http://www.ncbi.nlm.nih.gov/pubmed/?term="+ rows[dataObj].pmid
-              });
-            }
-            console.log(arr.length);
-              res.send((arr));
-            }
-          else{
-            console.log(err);
-          }
-        });
-
-      connection.on('error', function(err) {
-            res.json({"code" : 100, "status" : "Error in connection database"});
+            res.json({
+                "code": 100,
+                "status": "Error in connection database"
+            });
             return;
-      });
+        });
+    });
+}
+app.get('/q', function(req, res) {
+    handle_database(req, res);
 });
+
+app.get('/get-link', function(req, res) {
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            connection.release();
+            res.json({
+                "code": 100,
+                "status": "Error in connection database"
+            });
+            return;
+        }
+        console.log('connected as id ' + connection.threadId);
+
+        var where_clause = " ";
+        if (req.query.filter == "true") {
+            where_clause += " and year = " + req.query.year;
+        }
+
+        var queryEntries = [];
+        queryEntries.push(req.query.sub);
+        queryEntries.push(req.query.pred);
+        queryEntries.push(req.query.key);
+        connection.query("select s,p,o, triples.pmid, sentence, year from  triples, sentences, years where s = ? " + where_clause + " and p= ? and o= ? and triples.sent_id=sentences.sent_id and triples.pmid = years.pmid", (queryEntries), function(err, rows) {
+            connection.release();
+            if (!err) {
+                var arr = [];
+                for (var dataObj in rows) {
+                    arr.push({
+                        'key': rows[dataObj].sentence,
+                        'value': "http://www.ncbi.nlm.nih.gov/pubmed/?term=" + rows[dataObj].pmid,
+                        'queryEntries': queryEntries
+                    });
+                }
+                console.log(arr.length);
+                res.send((arr));
+            } else {
+                console.log(err);
+            }
+        });
+
+        connection.on('error', function(err) {
+            res.json({
+                "code": 100,
+                "status": "Error in connection database"
+            });
+            return;
+        });
+    });
 });
 
 //app.use('/', routes);
@@ -205,12 +221,12 @@ app.use(function(err, req, res, next) {
 });
 */
 app.get('/', function(req, res) {
-   res.sendFile(path.join(__dirname + '/views/index.html'));
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 app.get('/sqlOutput', function(req, res) {
-   handle_database(req,res);
+    handle_database(req, res);
 });
-app.listen(3000, function () {
-  console.log('Application now listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Application now listening on port 3000!');
 });
 module.exports = app;
